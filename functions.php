b6ea17ec5270cd7e1d753ea0680db010b64ebbce
anzuuu0905 <anzuuu@yahoo.co.jp>
2023-05-26 05:46:40 +0900

    CSS,JavaScriptの読み込み条件の変更

--- functions.php
+++ functions.php
@@ -2,12 +2,20 @@
 /**
  * Functions
  */
-// CSS読み込み
-wp_enqueue_style('my-style', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_theme_file_path('assets/css/style.css')),  'all');
 
-// JavaScript読み込み
-wp_enqueue_script('my-script', get_template_directory_uri() . '/assets/js/script.js', array(), filemtime(get_theme_file_path('assets/js/script.js')), true);
+/**
+ * CSSとJavaScriptの読み込み
+ *
+ * @codex https://wpdocs.osdn.jp/%E3%83%8A%E3%83%93%E3%82%B2%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%83%A1%E3%83%8B%E3%83%A5%E3%83%BC
+ */
+function my_script_init()
+{
+  // CSS読み込み
+  wp_enqueue_style('my-style', get_template_directory_uri() . '/assets/css/style.css', array(), filemtime(get_theme_file_path('assets/css/style.css')),  'all');
 
+  // JavaScript読み込み
+  wp_enqueue_script('my-script', get_template_directory_uri() . '/assets/js/script.js', array(), filemtime(get_theme_file_path('assets/js/script.js')), true);
+}
 /**
  * セキュリティー対策
  */
@@ -16,7 +24,7 @@ wp_enqueue_script('my-script', get_template_directory_uri() . '/assets/js/script
  * wordpressバージョン情報の削除
  * @see　https://qiita.com/Taka96/items/b541b1fef0fa20add47d
  */
-remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'wp_generator');
 
 /**
  * 投稿者一覧ページを自動で生成されないようにする
